fd999f7fe0652226ccca8b18dd7f3dd0ef228c6a
Rupert Amodia <rupertraphael.amodia@ucalgary.ca>
2020-11-18 00:33:12 -0700

    Assign 4: finish code.

--- assign4/assign4.asm
+++ assign4/assign4.asm
@@ -1,11 +1,13 @@
 // --------------------------------------------------------
-// Author:
-// Date:
-// Description:
+// Author: 	Rupert Raphael Amodia
+// Date:	November 18, 2020
+// Description:	Simple emulation of search engine
 // --------------------------------------------------------
 
 	.text
 	theD: .string "%-6d"
+	struct:	.string "document %d - highest frequency: %d (word %d)"
+	error:	.string "Invalid arguments.\n"
 	linebreak: .string "\n"	
 
 				// Eventually, registers for:
@@ -16,14 +18,20 @@ define(count_cells_r, x22)	// number of cells n*m
 define(cell_r, x23)		// current cell
 define(offset_r, x24)		// offset to get a table cell's address
 define(randNum_r, x25)		// generated random number
-define(table_base_r, x26)	// base address of table
+define(struct_alloc_r, x26)	// number of bytes needed to allocate for the struct
 define(row_r, x27)		// current row
 define(col_r, x28)		// current column
 
-define(maxocc_r, x22)		// maximum occurence
+define(table_base_r, x26)	// base address of table
+
+define(maxword_r, x21)		// number of bytes needed to allocate for table		
+define(maxoccur_r, x22)		// number of cells n*m
+define(struct_base_r, x23)	// current cell
+define(occur_r, x25)		// generated random number
+define(size_r, x9)
 
 TABLE_ELEMENT_SIZE = 8
-table_s = -16
+table_s = 0
 ALIGN = -16
 MAX_RAND = 16 - 1
 
@@ -33,37 +41,68 @@ MAX_RAND = 16 - 1
 main:	stp	x29,	x30,	[sp, -16]!
 	mov	x29,	sp
 
+	mov	x9,	3	// exact number of args
+	cmp	x0,	x9	// if not equal,
+	b.ne	invalidargs	// prompt invalid args and exit
+
+	ldr	x0, [x1, 8]	// load second argument to x0
+	ldr	n_r, [x1, 16]	// load third argument into register for number of columns
+	bl	atoi		// convert second argument into int
+	mov	m_r,	x0	// second argument is number of rows
+
+	
+	mov	x0,	n_r	// set third command line arg as first argument for atoi
+	bl 	atoi		// convert to int
+	mov	n_r,	x0	// third command line argument is number of columns
+
+	mov	x9,	4	// minimum number of rows/columns
+	mov	x10,	16	// maximum number of rows/columns
+	cmp	m_r,	x9	// if less than minimum number of rows,
+	b.lt	invalidargs	// prompt invalid args and exit
+	cmp	m_r,	x10	// if more than max number of rows,
+	b.gt	invalidargs	// prompt invalid args and exit
+	cmp	n_r,	x9	// if less than min number of cols
+	b.lt	invalidargs     // prompt invalid args and exit
+	cmp	n_r,	x10     // if more than max number of rows
+	b.gt	invalidargs     // prompt invalid args and exit
+
 	// Seed rand
 	mov	x0,	0
 	bl	time
 	bl	srand
 
-	// Temporarily set fixed m and n
-	mov	m_r,	4
-	mov	n_r,	4
-
 
 	// Calculate required space for table
-	// number of bytes allocated for table = 4 * m * n
-	mul	count_cells_r,		m_r,		n_r	
-	sub	table_alloc_r,		xzr,		count_cells_r
-	lsl	table_alloc_r,		table_alloc_r,	4
-	and	table_alloc_r,		table_alloc_r,	ALIGN
-	add	sp,	sp,	table_alloc_r
+	// number of bytes allocated for table = 8 * m * n
+	mul	count_cells_r,		m_r,		n_r		// number of rows * number of columns	
+	sub	table_alloc_r,		xzr,		count_cells_r	// negate table_alloc_r
+	lsl	table_alloc_r,		table_alloc_r,	3		// multiply by 8
+	and	table_alloc_r,		table_alloc_r,	-16		// make sure table_alloc_r is divisible by 16
 
 	// Calculate required space for struct
-	// number of bytes for struct = 
+	// number of bytes for struct = m * (8 + 8) = m * 2 * 8 = m * 16 
+	lsl	struct_alloc_r,		m_r,	4			// number of rows * 16	
+	sub	struct_alloc_r,		xzr,	struct_alloc_r		// make struct_alloc negative
+	and	struct_alloc_r,		struct_alloc_r,	-16		// make sure allocation is divisible by 16
 
-	add	x0,	x29,	table_s
-	mov	x1,	m_r
-	mov	x2,	n_r
+	add	sp,	sp,	table_alloc_r			// allocate space for table
+	add	sp,	sp,	struct_alloc_r			// allocate space for struct
+
+	add	x0,	x29,	table_s				// first arg is table's base address	
+	mov	x1,	m_r					// second arg is number of rows
+	mov	x2,	n_r					// third arg is number of cols
 	bl	generateTable
+
+	add	x0,	x29,	table_s				// set first arg as table's base address
+	mov	x1,	m_r					// second arg is number of rows
+	mov	x2,	n_r					// third arg is number of cols
+	add	x3,	x0,	table_alloc_r			// last arg is struct's base address (struct is right after table)
+	bl	generateStruct
 
 	mov	cell_r,		xzr		// start at cell 0
 	mov	offset_r,	table_s		// offset for x29 - start at table base address
 	mov	col_r,		xzr		// start column at 0
 	mov	row_r,		xzr		// start row at 0
-	mov	maxocc_r,	xzr		// max occurence is set to 0
 
 print:
 	ldr	randNum_r,	[x29, offset_r]		// load table cell value at offset
@@ -75,13 +114,6 @@ print:
 
 	sub     offset_r,       offset_r,       TABLE_ELEMENT_SIZE	// decrement offset by table element size
 
-	// if table cell value is greater than
-	// currently stored max occurence,
-	// set it as the max occurence 
-	cmp	randNum_r,	maxocc_r
-	b.le	inc_col				// otherwise, skip
-	mov	maxocc_r,	randNum_r	// set new max occurence
-
 inc_col:
 	add	col_r,		col_r,		1			// increment column number to keep track of current table cell column
 
@@ -89,18 +121,10 @@ inc_col:
         b.lt    print							// loop
 
 	// otherwise, reset column to 0, go to new row
-	ldr	x0,	=linebreak
-	bl	printf
-	mov	col_r,	xzr
+	ldr	x0,	=linebreak					// for new row,
+	bl	printf							// print line break
+	mov	col_r,	xzr						// reset column to 0
 	
-	//
-	// store max frequency for document to struct
-
-	mov	maxocc_r,	xzr		// new row, so set max occurence back to zero
-
-	ldr	x0,	=linebreak
-	bl	printf
-
 	add 	row_r,		row_r,		1			// increment row number to keep track of current table cell row
 	cmp	row_r,		m_r					// if row < number of rows:
 	b.lt	print							// loop
@@ -109,13 +133,50 @@ inc_col:
 	// this row and column does not exist in the table so the printing loop
 	// has been completely executed. 	
 
+	mov	row_r,		xzr			// start at row 0
+printStruct:
+	ldr	x0,	=linebreak
+	bl	printf
+
+	lsl	offset_r,	row_r,	4
+	sub	offset_r,	table_alloc_r, offset_r			// offset for max word = table_alloc_r - (row * 2) * 8 
+	ldr	x3,	[x29, offset_r]
+
+	lsl	offset_r,	row_r,	1
+	add	offset_r,	offset_r,	1	// offset for max freq = table_alloc_r - (row * 2 + 1) * 8
+	lsl	offset_r,	offset_r,	3	// multiply by 8 (as per above)
+	sub	offset_r,	table_alloc_r,	offset_r// subtract offset from table_alloc which is the offset (from fp) base address for struct
+
+	ldr	x2,	[x29, offset_r]			// load highest frequency(struct.frequency) for printing
+
+	ldr	x0,	=struct				// load string format for printing struct
+	mov	x1,	row_r				// add row/document number to printing
+	bl	printf					// print struct info
+
+	add	row_r,	row_r,	1			// keep track of current row
+	cmp	row_r,	m_r				// if row < number of rows,	
+	b.lt	printStruct				// loop printing struct data
+
+	ldr	x0,	=linebreak
+	bl	printf		
+
 	// Deallocate space used by table
-	sub	sp,	sp,	table_alloc_r
+	sub	sp,	sp,	table_alloc_r		// deallocate memory used for table
+	sub	sp,	sp,	struct_alloc_r		// deallocate memory used for struct
+	b	exitMain				// jump to exit; dont prompt invalid args
+			
 
-exitMain:	ldp	x29,	x30,	[sp], 16
-		ret
+invalidargs:
+	ldr	x0,	=error
+	bl	printf	
 
+exitMain:	
+	ldp	x29,	x30,	[sp], 16
+	ret
 
+// macro for calculating needed memory and address offsets
+// for temporarily storing caller-saved registers
+// into memory
 define(
 	init_subr_x,
 	`alloc = -(16 + 8 * 9) & -16
@@ -133,6 +194,10 @@ define(
 	x28_s = x27_s + 8'
 )
 
+// macro for storing caller-saved register values 
+// into memory
+// Ideally should be used after initializing subroutine FP
+// and before using caller-saved registers
 define(
 	str_x,	
 	`str	x19,	[x29, x19_s]
@@ -147,6 +212,9 @@ define(
 	str	x28,	[x29, x28_s]'
 )
 
+// macro for restoring caller-saved register
+// values from memory
+// Ideally should be used before restoring frame pointer and stack pointer and returning.
 define(
 	ldr_x,
 	`ldr	x19,	[x29, x19_s]
@@ -164,42 +232,130 @@ define(
 init_subr_x()
 
 // generateTable(&table, numrows, numcols)
+// populates random occurences (0 - 15) into the given table
 generateTable:
 	stp	x29,	x30,	[sp, alloc]!
 	mov	x29,	sp
 
-	str_x()
+	str_x()		// store caller-saved register values
 
-	mov	table_base_r,	x0		
+	mov	table_base_r,	x0	// get table base address from given arguments	
 
-	mul	count_cells_r,		x1,	x2
+	mul	count_cells_r,		x1,	x2	 // calculate number of cells the table has
 	
-	mov	row_r,		xzr
-	mov	col_r,		xzr
-	mov	offset_r,	xzr
-	mov	cell_r,		xzr
+	mov	offset_r,	xzr		// start offset for table base address at 0
+	mov	cell_r,		xzr		// start at cell 0 
 loop:
 	// Store random numbers in each table cell
 	// First, we generate the random number
 	bl	rand				// Generate random number
 	mov	randNum_r,	x0
 
-	and	randNum_r,	randNum_r,	MAX_RAND		
+	and	randNum_r,	randNum_r,	MAX_RAND	// limit number from 0 - MAX_RAND		
 
-	str	randNum_r,	[table_base_r, offset_r]
+	str	randNum_r,	[table_base_r, offset_r]	// store number in array
 
-	sub	offset_r,	offset_r,	TABLE_ELEMENT_SIZE
-	add	cell_r,		cell_r,		1
+	sub	offset_r,	offset_r,	TABLE_ELEMENT_SIZE	// keep track of offset for where to store number in stack
+	add	cell_r,		cell_r,		1		// keep track of cell number
 
 	// Loop until current cell number = number of cells
 	cmp	cell_r,		count_cells_r		
 	b.lt	loop
+	
+	ldr_x()	
+
+	ldp	x29,	x30,	[sp], dealloc
+	ret
+
+init_subr_x()
+alloc = (alloc - 8) & -16 	// will be allocating extra 8 bytes to store the size of current row
+dealloc = -alloc
+size_s = x28_s + 8		// position of size value relative to frame pointer 
+
+
+// generateStruct(&table, numcols, numrows, &struct)
+// populates an array of structs (with attrs word with highest frequency and highest frequency)
+// each struct represents each document and by convention, their indices match
+// e.g. structs[0] represents table[0]
+generateStruct:
+	stp	x29,	x30,	[sp, alloc]!
+	mov	x29,	sp
+
+	str_x()
+	str	xzr,	[x29, size_s]	// initialize size to 0
+
+	mov	table_base_r,	x0	// get table base from args
+	mov	m_r,		x1	// get number of rows from args
+	mov	n_r,		x2	// get number of cols from args
+	mov	struct_base_r,	x3	// get struct base from args			
+
+	mov	row_r,		xzr	// start at row/doc 0
+	mov	col_r,		xzr	// start at col/word 0
+	mov	offset_r,	xzr	// start at 0 offset
+	mov	maxword_r,	col_r	// highest frequency word is the first one, for now
+	mov	maxoccur_r,	xzr	// highest frequency is zero, for now
+
+structloop:
+	// calculate table offset = (row * numcols + col) * 8
+	mul	offset_r,	row_r,		n_r		// row * numcols
+	add	offset_r,	offset_r,	col_r		// row * numcols + col
+	lsl	offset_r,	offset_r,	3		// 8 * (rows * numcols + col )
+	sub	offset_r,	xzr,		offset_r	// negate offset
+	ldr	occur_r,	[table_base_r, offset_r]	// load occurence from table
+
+	// to replace or not to replace max occurence and max word
+	cmp	occur_r,	maxoccur_r	// don't replace max occurence
+	b.le	addsize				// if current occurence isn't greater than max occurence
+	mov	maxoccur_r,	occur_r		// set new max occurence	
+	mov	maxword_r,	col_r		// set new word with highest occurence/frequency
+
+addsize:
+	// add to size for document
+	ldr	size_r,		[x29, size_s]	
+	add	size_r,		size_r,		occur_r		// add occurence to total size for document	
+	str	size_r,		[x29, size_s]			// store size to memory
+
+	add	col_r,	col_r,	1	// increment column
+	
+	cmp	col_r,	n_r	// if column < numcols, 
+	b.lt	structloop	// loop
+	mov	col_r,	xzr	// else reset column,
+
+	// store index and frequency in struct
+	// calculate offset for struct
+	lsl	offset_r,	row_r,	4			// calculate offset for document index	
+	sub	offset_r,	xzr,	offset_r		// make offset negative
+	str	maxword_r,	[struct_base_r, offset_r]	// store document index to struct
+	lsl	offset_r,	row_r,	1			// calculate offset for frequency
+	add	offset_r,	offset_r,	1
+	lsl	offset_r,	offset_r,	3
+	sub	offset_r,	xzr,	offset_r		// make offset negative
+
+	mov	x0,	maxoccur_r		// set maxoccur as first argument
+	ldr	x1,	[x29, size_s]		// set size as second argument
+	bl	calculateFrequency
+	mov	maxoccur_r,	x0		// replace maxoccur with calculated frequency
+
+	str	maxoccur_r,	[struct_base_r, offset_r]	// store frequency to struct
+
+	mov	maxword_r,	xzr	// reset max word
+	mov	maxoccur_r,	xzr	// reset max occurence
+	str	xzr,		[x29, size_s] // reset size	
+	mov 	size_r,		xzr
+
+	// increment row
+	add	row_r,	row_r,	1	
+
+	// if row < numrows, loop 
+	cmp	row_r,	m_r
+	b.lt	structloop
 	
 	ldr_x()
 
 	ldp	x29,	x30,	[sp], dealloc
 	ret
 
+init_subr_x()
 // frequency = word occurences in doc * 100 / size of doc
 // calculateFrequency(occurences, size)
 calculateFrequency:
@@ -208,11 +364,12 @@ calculateFrequency:
 
 	str_x()
 
+	mov	x9,	100
+
 	// Multiply by 100
-
+	mul	x0,	x0,	x9	
 	// Divide
-
-	// Store to x0
+	udiv	x0,	x0,	x1
 
 	ldr_x()
 
--- assign4/assign4.s
+++ assign4/assign4.s
@@ -1,11 +1,13 @@
 // --------------------------------------------------------
-// Author:
-// Date:
-// Description:
+// Author: 	Rupert Raphael Amodia
+// Date:	November 18, 2020
+// Description:	Simple emulation of search engine
 // --------------------------------------------------------
 
 	.text
 	theD: .string "%-6d"
+	struct:	.string "document %d - highest frequency: %d (word %d)"
+	error:	.string "Invalid arguments.\n"
 	linebreak: .string "\n"	
 
 				// Eventually, registers for:
@@ -16,14 +18,20 @@
 		// current cell
 		// offset to get a table cell's address
 		// generated random number
-	// base address of table
+	// number of bytes needed to allocate for the struct
 		// current row
 		// current column
 
-		// maximum occurence
+	// base address of table
+
+		// number of bytes needed to allocate for table		
+		// number of cells n*m
+	// current cell
+		// generated random number
+
 
 TABLE_ELEMENT_SIZE = 8
-table_s = -16
+table_s = 0
 ALIGN = -16
 MAX_RAND = 16 - 1
 
@@ -33,37 +41,68 @@ MAX_RAND = 16 - 1
 main:	stp	x29,	x30,	[sp, -16]!
 	mov	x29,	sp
 
+	mov	x9,	3	// exact number of args
+	cmp	x0,	x9	// if not equal,
+	b.ne	invalidargs	// prompt invalid args and exit
+
+	ldr	x0, [x1, 8]	// load second argument to x0
+	ldr	x20, [x1, 16]	// load third argument into register for number of columns
+	bl	atoi		// convert second argument into int
+	mov	x19,	x0	// second argument is number of rows
+
+	
+	mov	x0,	x20	// set third command line arg as first argument for atoi
+	bl 	atoi		// convert to int
+	mov	x20,	x0	// third command line argument is number of columns
+
+	mov	x9,	4	// minimum number of rows/columns
+	mov	x10,	16	// maximum number of rows/columns
+	cmp	x19,	x9	// if less than minimum number of rows,
+	b.lt	invalidargs	// prompt invalid args and exit
+	cmp	x19,	x10	// if more than max number of rows,
+	b.gt	invalidargs	// prompt invalid args and exit
+	cmp	x20,	x9	// if less than min number of cols
+	b.lt	invalidargs     // prompt invalid args and exit
+	cmp	x20,	x10     // if more than max number of rows
+	b.gt	invalidargs     // prompt invalid args and exit
+
 	// Seed rand
 	mov	x0,	0
 	bl	time
 	bl	srand
 
-	// Temporarily set fixed m and n
-	mov	x19,	4
-	mov	x20,	4
-
 
 	// Calculate required space for table
-	// number of bytes allocated for table = 4 * m * n
-	mul	x22,		x19,		x20	
-	sub	x21,		xzr,		x22
-	lsl	x21,		x21,	4
-	and	x21,		x21,	ALIGN
-	add	sp,	sp,	x21
+	// number of bytes allocated for table = 8 * m * n
+	mul	x22,		x19,		x20		// number of rows * number of columns	
+	sub	x21,		xzr,		x22	// negate x21
+	lsl	x21,		x21,	3		// multiply by 8
+	and	x21,		x21,	-16		// make sure x21 is divisible by 16
 
 	// Calculate required space for struct
-	// number of bytes for struct = 
+	// number of bytes for struct = m * (8 + 8) = m * 2 * 8 = m * 16 
+	lsl	x26,		x19,	4			// number of rows * 16	
+	sub	x26,		xzr,	x26		// make struct_alloc negative
+	and	x26,		x26,	-16		// make sure allocation is divisible by 16
 
-	add	x0,	x29,	table_s
-	mov	x1,	x19
-	mov	x2,	x20
+	add	sp,	sp,	x21			// allocate space for table
+	add	sp,	sp,	x26			// allocate space for struct
+
+	add	x0,	x29,	table_s				// first arg is table's base address	
+	mov	x1,	x19					// second arg is number of rows
+	mov	x2,	x20					// third arg is number of cols
 	bl	generateTable
+
+	add	x0,	x29,	table_s				// set first arg as table's base address
+	mov	x1,	x19					// second arg is number of rows
+	mov	x2,	x20					// third arg is number of cols
+	add	x3,	x0,	x21			// last arg is struct's base address (struct is right after table)
+	bl	generateStruct
 
 	mov	x23,		xzr		// start at cell 0
 	mov	x24,	table_s		// offset for x29 - start at table base address
 	mov	x28,		xzr		// start column at 0
 	mov	x27,		xzr		// start row at 0
-	mov	x22,	xzr		// max occurence is set to 0
 
 print:
 	ldr	x25,	[x29, x24]		// load table cell value at offset
@@ -75,13 +114,6 @@ print:
 
 	sub     x24,       x24,       TABLE_ELEMENT_SIZE	// decrement offset by table element size
 
-	// if table cell value is greater than
-	// currently stored max occurence,
-	// set it as the max occurence 
-	cmp	x25,	x22
-	b.le	inc_col				// otherwise, skip
-	mov	x22,	x25	// set new max occurence
-
 inc_col:
 	add	x28,		x28,		1			// increment column number to keep track of current table cell column
 
@@ -89,18 +121,10 @@ inc_col:
         b.lt    print							// loop
 
 	// otherwise, reset column to 0, go to new row
-	ldr	x0,	=linebreak
-	bl	printf
-	mov	x28,	xzr
+	ldr	x0,	=linebreak					// for new row,
+	bl	printf							// print line break
+	mov	x28,	xzr						// reset column to 0
 	
-	ldr	x0,	=theD			// load string format and use as argument for printing
-	mov	x1,	x22		// use loaded table cell value as argument for printing
-	bl	printf
-	mov	x22,	xzr		// new row, so set max occurence back to zero
-
-	ldr	x0,	=linebreak
-	bl	printf
-
 	add 	x27,		x27,		1			// increment row number to keep track of current table cell row
 	cmp	x27,		x19					// if row < number of rows:
 	b.lt	print							// loop
@@ -109,17 +133,61 @@ inc_col:
 	// this row and column does not exist in the table so the printing loop
 	// has been completely executed. 	
 
+	mov	x27,		xzr			// start at row 0
+printStruct:
+	ldr	x0,	=linebreak
+	bl	printf
+
+	lsl	x24,	x27,	4
+	sub	x24,	x21, x24			// offset for max word = x21 - (row * 2) * 8 
+	ldr	x3,	[x29, x24]
+
+	lsl	x24,	x27,	1
+	add	x24,	x24,	1	// offset for max freq = x21 - (row * 2 + 1) * 8
+	lsl	x24,	x24,	3	// multiply by 8 (as per above)
+	sub	x24,	x21,	x24// subtract offset from table_alloc which is the offset (from fp) base address for struct
+
+	ldr	x2,	[x29, x24]			// load highest frequency(struct.frequency) for printing
+
+	ldr	x0,	=struct				// load string format for printing struct
+	mov	x1,	x27				// add row/document number to printing
+	bl	printf					// print struct info
+
+	add	x27,	x27,	1			// keep track of current row
+	cmp	x27,	x19				// if row < number of rows,	
+	b.lt	printStruct				// loop printing struct data
+
+	ldr	x0,	=linebreak
+	bl	printf		
+
 	// Deallocate space used by table
-	sub	sp,	sp,	x21
+	sub	sp,	sp,	x21		// deallocate memory used for table
+	sub	sp,	sp,	x26		// deallocate memory used for struct
+	b	exitMain				// jump to exit; dont prompt invalid args
+			
 
-exitMain:	ldp	x29,	x30,	[sp], 16
-		ret
+invalidargs:
+	ldr	x0,	=error
+	bl	printf	
+
+exitMain:	
+	ldp	x29,	x30,	[sp], 16
+	ret
+
+// macro for calculating needed memory and address offsets
+// for temporarily storing caller-saved registers
+// into memory
 
 
+// macro for storing caller-saved register values 
+// into memory
+// Ideally should be used after initializing subroutine FP
+// and before using caller-saved registers
 
 
-
-
+// macro for restoring caller-saved register
+// values from memory
+// Ideally should be used before restoring frame pointer and stack pointer and returning.
 
 
 alloc = -(16 + 8 * 9) & -16
@@ -138,6 +206,7 @@ alloc = -(16 + 8 * 9) & -16
 
 
 // generateTable(&table, numrows, numcols)
+// populates random occurences (0 - 15) into the given table
 generateTable:
 	stp	x29,	x30,	[sp, alloc]!
 	mov	x29,	sp
@@ -152,28 +221,26 @@ generateTable:
 	str	x26,	[x29, x26_s]
 	str	x27,	[x29, x27_s]
 	str	x28,	[x29, x28_s]
+		// store caller-saved register values
 
+	mov	x26,	x0	// get table base address from given arguments	
 
-	mov	x26,	x0		
-
-	mul	x22,		x1,	x2
+	mul	x22,		x1,	x2	 // calculate number of cells the table has
 	
-	mov	x27,		xzr
-	mov	x28,		xzr
-	mov	x24,	xzr
-	mov	x23,		xzr
+	mov	x24,	xzr		// start offset for table base address at 0
+	mov	x23,		xzr		// start at cell 0 
 loop:
 	// Store random numbers in each table cell
 	// First, we generate the random number
 	bl	rand				// Generate random number
 	mov	x25,	x0
 
-	and	x25,	x25,	MAX_RAND		
+	and	x25,	x25,	MAX_RAND	// limit number from 0 - MAX_RAND		
 
-	str	x25,	[x26, x24]
+	str	x25,	[x26, x24]	// store number in array
 
-	sub	x24,	x24,	TABLE_ELEMENT_SIZE
-	add	x23,		x23,		1
+	sub	x24,	x24,	TABLE_ELEMENT_SIZE	// keep track of offset for where to store number in stack
+	add	x23,		x23,		1		// keep track of cell number
 
 	// Loop until current cell number = number of cells
 	cmp	x23,		x22		
@@ -189,10 +256,145 @@ loop:
 	ldr	x26,	[x29, x26_s]
 	ldr	x27,	[x29, x27_s]
 	ldr	x28,	[x29, x28_s]
+	
+
+	ldp	x29,	x30,	[sp], dealloc
+	ret
+
+alloc = -(16 + 8 * 9) & -16
+	dealloc = -alloc
+
+	x19_s = 16
+	x20_s = x19_s + 8
+	x21_s = x20_s + 8 
+	x22_s = x21_s + 8
+	x23_s = x22_s + 8
+	x24_s = x23_s + 8
+	x25_s = x24_s + 8
+	x26_s = x25_s + 8
+	x27_s = x26_s + 8
+	x28_s = x27_s + 8
+
+alloc = (alloc - 8) & -16 	// will be allocating extra 8 bytes to store the size of current row
+dealloc = -alloc
+size_s = x28_s + 8		// position of size value relative to frame pointer 
+
+
+// generateStruct(&table, numcols, numrows, &struct)
+// populates an array of structs (with attrs word with highest frequency and highest frequency)
+// each struct represents each document and by convention, their indices match
+// e.g. structs[0] represents table[0]
+generateStruct:
+	stp	x29,	x30,	[sp, alloc]!
+	mov	x29,	sp
+
+	str	x19,	[x29, x19_s]
+	str	x20,	[x29, x20_s]
+	str	x21,	[x29, x21_s]
+	str	x22,	[x29, x22_s]
+	str	x23,	[x29, x23_s]
+	str	x24,	[x29, x24_s]
+	str	x25,	[x29, x25_s]
+	str	x26,	[x29, x26_s]
+	str	x27,	[x29, x27_s]
+	str	x28,	[x29, x28_s]
+
+	str	xzr,	[x29, size_s]	// initialize size to 0
+
+	mov	x26,	x0	// get table base from args
+	mov	x19,		x1	// get number of rows from args
+	mov	x20,		x2	// get number of cols from args
+	mov	x23,	x3	// get struct base from args			
+
+	mov	x27,		xzr	// start at row/doc 0
+	mov	x28,		xzr	// start at col/word 0
+	mov	x24,	xzr	// start at 0 offset
+	mov	x21,	x28	// highest frequency word is the first one, for now
+	mov	x22,	xzr	// highest frequency is zero, for now
+
+structloop:
+	// calculate table offset = (row * numcols + col) * 8
+	mul	x24,	x27,		x20		// row * numcols
+	add	x24,	x24,	x28		// row * numcols + col
+	lsl	x24,	x24,	3		// 8 * (rows * numcols + col )
+	sub	x24,	xzr,		x24	// negate offset
+	ldr	x25,	[x26, x24]	// load occurence from table
+
+	// to replace or not to replace max occurence and max word
+	cmp	x25,	x22	// don't replace max occurence
+	b.le	addsize				// if current occurence isn't greater than max occurence
+	mov	x22,	x25		// set new max occurence	
+	mov	x21,	x28		// set new word with highest occurence/frequency
+
+addsize:
+	// add to size for document
+	ldr	x9,		[x29, size_s]	
+	add	x9,		x9,		x25		// add occurence to total size for document	
+	str	x9,		[x29, size_s]			// store size to memory
+
+	add	x28,	x28,	1	// increment column
+	
+	cmp	x28,	x20	// if column < numcols, 
+	b.lt	structloop	// loop
+	mov	x28,	xzr	// else reset column,
+
+	// store index and frequency in struct
+	// calculate offset for struct
+	lsl	x24,	x27,	4			// calculate offset for document index	
+	sub	x24,	xzr,	x24		// make offset negative
+	str	x21,	[x23, x24]	// store document index to struct
+	lsl	x24,	x27,	1			// calculate offset for frequency
+	add	x24,	x24,	1
+	lsl	x24,	x24,	3
+	sub	x24,	xzr,	x24		// make offset negative
+
+	mov	x0,	x22		// set maxoccur as first argument
+	ldr	x1,	[x29, size_s]		// set size as second argument
+	bl	calculateFrequency
+	mov	x22,	x0		// replace maxoccur with calculated frequency
+
+	str	x22,	[x23, x24]	// store frequency to struct
+
+	mov	x21,	xzr	// reset max word
+	mov	x22,	xzr	// reset max occurence
+	str	xzr,		[x29, size_s] // reset size	
+	mov 	x9,		xzr
+
+	// increment row
+	add	x27,	x27,	1	
+
+	// if row < numrows, loop 
+	cmp	x27,	x19
+	b.lt	structloop
+	
+	ldr	x19,	[x29, x19_s]
+	ldr	x20,	[x29, x20_s]
+	ldr	x21,	[x29, x21_s]
+	ldr	x22,	[x29, x22_s]
+	ldr	x23,	[x29, x23_s]
+	ldr	x24,	[x29, x24_s]
+	ldr	x25,	[x29, x25_s]
+	ldr	x26,	[x29, x26_s]
+	ldr	x27,	[x29, x27_s]
+	ldr	x28,	[x29, x28_s]
 
 
 	ldp	x29,	x30,	[sp], dealloc
 	ret
+
+alloc = -(16 + 8 * 9) & -16
+	dealloc = -alloc
+
+	x19_s = 16
+	x20_s = x19_s + 8
+	x21_s = x20_s + 8 
+	x22_s = x21_s + 8
+	x23_s = x22_s + 8
+	x24_s = x23_s + 8
+	x25_s = x24_s + 8
+	x26_s = x25_s + 8
+	x27_s = x26_s + 8
+	x28_s = x27_s + 8
 
 // frequency = word occurences in doc * 100 / size of doc
 // calculateFrequency(occurences, size)
@@ -212,11 +414,12 @@ calculateFrequency:
 	str	x28,	[x29, x28_s]
 
 
+	mov	x9,	100
+
 	// Multiply by 100
-
+	mul	x0,	x0,	x9	
 	// Divide
-
-	// Store to x0
+	udiv	x0,	x0,	x1
 
 	ldr	x19,	[x29, x19_s]
 	ldr	x20,	[x29, x20_s]
